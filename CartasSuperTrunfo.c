171154f5cf9d3505f4ccfecb0a0ddc474b0c5094
carisson <202603069036@alunos.estacio.br>
2026-03-11 01:08:26 +0000

    Criei  variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,12 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+char estado, estado1;
+char carta[3], carta1[3];
+char cidade[20], cidade1[20];
+float area, area1, pib, pib1;
+int populacao, populacao1, turisticos, turisticos1;
+float densidade, percapita, percapita1, densidade1;
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
