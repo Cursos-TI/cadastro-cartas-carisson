7ac00a7e65da314ac18018fe202117ab5b4f063f
carisson <202603069036@alunos.estacio.br>
2026-03-11 01:12:59 +0000

    coleta de dados definido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,7 +13,56 @@ float area, area1, pib, pib1;
 int populacao, populacao1, turisticos, turisticos1;
 float densidade, percapita, percapita1, densidade1;
   // Área para entrada de dados
+printf("Dados da carta 1\n");
+    printf("Digite o estado: ");
+    scanf("%c", &estado);
 
+    printf("Digite o código da carta: ");
+    scanf("%s", &carta);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &cidade);
+
+    printf("Digite a quantidade de habitantes: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a area em km²: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib);
+
+    printf("Digite a quantidade de pontos turisticos:\n");
+    scanf("%d", &turisticos);
+
+
+    printf("Dados da carta 2\n ");
+
+    printf("Digite o estado: ");
+    scanf("%c", &estado1);
+
+    printf("Digite o código da carta: ");
+    scanf("%s", &carta1);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &cidade1);
+
+    printf("Digite a quantidade de habitantes: ");
+    scanf("%d", &populacao1);
+
+    printf("Digite a area em km²: ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Digite a quantidade de pontos turisticos:\n");
+    scanf("%d", &turisticos1);
+
+    densidade = populacao / area;
+    percapita = (float) pib / populacao;
+    densidade1 = populacao1 / area1;
+    percapita1 = (float) pib1 / populacao1;
   // Área para exibição dos dados da cidade
 
 return 0;
